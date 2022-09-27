1c20ce8e6d46e1f19afa380124f7117544c38d3d
Felipe López <jualopezra@unal.edu.co>
2022-09-27 00:22:34 -0500

    aaa

--- src/main/java/zooAnimales/Mamifero.java
+++ src/main/java/zooAnimales/Mamifero.java
@@ -1,20 +1,20 @@
 package zooAnimales;
 import java.util.ArrayList;
-public class Mamifero {
-	private ArrayList<Mamifero> listado=new ArrayList<Mamifero>();
-	public  static int caballos = 0;
-	public  static int leones = 0;
-	private boolean pelaje;
-	private int patas;
-	
-	public Mamifero() {
-		this(null, 0, null, null, false, 0);
-	}
-	public Mamifero(String nombre, int edad, String habitat, String genero, boolean pelaje, int patas) {
-		super(nombre, edad, habitat, genero); 
-		this.pelaje = pelaje;
-		this.patas =  patas;
-	}
+public class Mamifero extends Animal {
+	private static ArrayList<Mamifero> listado=new ArrayList<Mamifero>();
+    public static int caballos;
+    public static int leones;
+    private boolean pelaje;
+    private int patas;
+    public Mamifero() {
+        this(null, 0, null, null, false, 0);
+    }  
+    
+    public Mamifero(String nombre, int edad, String habitat, String genero,boolean pelaje, int patas) {
+        super(nombre, edad, habitat, genero);
+        this.pelaje = pelaje;
+        this.patas = patas;
+    }
 
 	
 	public int cantidadMamiferos() {
